acd9562ccd378f49e9937e3e4f4381ca7838cbd8
Hoyun Jung <65343417+stemmmm@users.noreply.github.com>
2023-04-11 17:17:47 +0900

    불필요한 파일 삭제
    - SceneDelegate 삭제
    - Info.plist Application Scene Manifest 제거
    - ViewController 제거

---
+++ RaiseUp/Application/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  RaiseUp
+//
+//  Created by 정호윤 on 2023/03/23.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
