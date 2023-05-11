fe0eccd945b41fe47ac298257eafcfd317439ab5
Hoyun Jung <65343417+stemmmm@users.noreply.github.com>
2023-05-11 16:49:36 +0900

    뷰 모델 fetchCampaigns 함수 수정
    - completion handler 삭제
    - 캠페인을 불러오면 클로저 실행하도록 변경

--- RaiseUp/ViewModel/CampaignListViewModel.swift
+++ RaiseUp/ViewModel/CampaignListViewModel.swift
@@ -12,19 +12,22 @@ final class CampaignListViewModel {
     private let firestoreService: DatabaseServiceType
     private(set) var campaigns: [Campaign] = []
     
+    var onCampaignsUpdated: (() -> Void)?
+    
     init(user: User = User(email: "email", password: "1234", name: "name"), firestoreService: DatabaseServiceType) {
         self.user = user
         self.firestoreService = firestoreService
     }
     
-    func fetchCampaigns(completion: @escaping ((Result<Void, Error>) -> Void)) {
+    func fetchCampaigns() {
         firestoreService.fetchDocuments(from: FirestoreCollection.campaigns.rawValue) { [weak self] (result: Result<[Campaign], Error>) in
             switch result {
             case .success(let campaigns):
                 self?.campaigns = campaigns
-                completion(.success(()))
+                self?.onCampaignsUpdated?()
             case .failure(let error):
-                completion(.failure(error))
+                // TODO: 에러 처리
+                print(error)
             }
         }
     }
